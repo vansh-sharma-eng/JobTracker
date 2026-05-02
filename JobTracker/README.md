1d2bb503628b18bc865c388ef09010f060426f7a
Vansh Sharma <vanshsharmas92005@gmail.com>
2026-05-02 10:22:48 +0530

    Updated project files

--- JobTracker/README.md
+++ JobTracker/README.md
@@ -1,1 +1,24 @@
+<<<<<<< HEAD
 
+=======
+# JobTracker
+
+A simple app to track job applications and their status.
+
+## Features
+
+* Add and manage jobs
+* Track status (Applied, Interview, etc.)
+
+## Tech Stack
+
+* React
+* JavaScript
+
+## Run Locally
+
+```bash
+npm install
+npm run dev
+```
+>>>>>>> dbd81aa (Updated project files)
--- JobTracker/src/App.jsx
+++ JobTracker/src/App.jsx
@@ -1,71 +1,75 @@
-// import React from 'react'
-// import Sidebar from './components/Sidebar'
-// import Header from './components/Header'
-// import StatsSection from './components/StatsSection'
-// import JobSection from './components/JobSection'
-// import FilterTabs from './components/FilterTabs'
-// import StepOne from './components/StepOne'
-// const App = () => {
+import React, { useState } from 'react'
+import Sidebar from './components/Sidebar'
+import Header from './components/Header'
+import StatsSection from './components/StatsSection'
+import JobSection from './components/JobSection'
+import FilterTabs from './components/FilterTabs'
+import AddJobModal from './components/AddJobForm'
 
-//   const JobCard = [
-    
-// {
-//       logo: "logo",
-//       companyname: "meta",
-//       post: "soft ware",
-//       location: "no remote",
-//       Date: "25-july-2026",
-//       Salary: "50",
-//       status: "Offer",
-//     },
-//   ]
-
-//   const Nocard = JobCard.length;
-
-//   const interview = JobCard.filter(job => job.status === "Interview").length;
-//   const Applied = JobCard.filter(job => job.status === "Applied").length;
-// const Offer = JobCard.filter(job => job.status === "Offer").length;
-// const Rejected = JobCard.filter(job => job.status === "Rejected").length;
-// const successRate = ((interview / Nocard) * 100).toFixed(1);
-//   return (
-//     <div className="flex h-[900px] bg-[#0a0d12] select-none">
-
-//       <Sidebar />
-
-//       <div className="flex-1 flex flex-col">
-//         <Header />
-
-//         <div className="p-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
-//           <StatsSection interview={interview} Nocard = {Nocard}
-//         Offer = {Offer} successRate ={successRate}/>
-//         </div>
-
-//         <FilterTabs Nocard={Nocard} interview={interview} Applied = {Applied}
-//         Offer = {Offer}
-//         Rejected = {Rejected} />
-
-//         <div className="ml-7">
-//           <JobSection JobCard={JobCard} />
-//         </div>
-
-//       </div>
-      
-
-
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-import React from 'react'
-import StepOne from "./components/StepOne"
-import StepTwo from './components/StepTwo'
 const App = () => {
+
+  const [openModal, setOpenModal] = useState(false);
+
+    const [jobs, setJobs] = useState([]);
+
+ 
+  const addJob = (newJob) => {
+    setJobs([...jobs, newJob]);
+  };
+
+
+  const Nocard = jobs.length;
+  const interview = jobs.filter(job => job.status === "Interview").length;
+  const Applied = jobs.filter(job => job.status === "Applied").length;
+  const Offer = jobs.filter(job => job.status === "Offer").length;
+  const Rejected = jobs.filter(job => job.status === "Rejected").length;
+  const successRate = Nocard === 0 ? 0 : ((interview / Nocard) * 100).toFixed(1);
+
   return (
-    <div>
-    <StepOne/>
+    <div className="flex h-full bg-[#0a0d12] select-none">
+
+      <Sidebar />
+
+      <div className="flex-1 flex flex-col">
+        
+     
+        <Header onAddClick={() => setOpenModal(true)}
+        Nocard={Nocard} interview={interview} Offer={Offer} />
+
+      
+        <div className="p-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
+          <StatsSection 
+            interview={interview} 
+            Nocard={Nocard}
+            Offer={Offer} 
+            successRate={successRate}
+          />
+        </div>
+
+ 
+        <FilterTabs 
+          Nocard={Nocard} 
+          interview={interview} 
+          Applied={Applied}
+          Offer={Offer}
+          Rejected={Rejected} 
+        />
+
+     
+        <div className="ml-7">
+          <JobSection JobCard={jobs} />
+        </div>
+
+      </div>
+
+    
+      {openModal && (
+        <AddJobModal 
+          onClose={() => setOpenModal(false)} 
+          onAddJob={addJob}
+        />
+      )}
+
     </div>
   )
 }
